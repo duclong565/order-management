b05a9eea79df7bef8ccce8722a81afa93e8da1f9
Dean.Nguyen <Dean.Nguyen@cebupacific.com>
2026-07-15 10:54:30 +0700

    Initial commit: Spring Boot order management scaffold

---
+++ src/test/java/com/example/order_management/TestOrderManagementApplication.java
@@ -0,0 +1,11 @@
+package com.example.order_management;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestOrderManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(OrderManagementApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
